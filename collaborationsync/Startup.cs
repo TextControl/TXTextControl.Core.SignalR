012b52ec643ef573ea329bb95e71dab64e0545d1
Zuzanna Kwiatkowska <zuzanna.kwiatkowska@textcontrol.com>
2023-12-13 14:13:57 +0100

    Text Control version updated to 32, .NET version updated to 6.0

--- collaborationsync/Startup.cs
+++ collaborationsync/Startup.cs
@@ -61,14 +61,7 @@ namespace collaborationsync
             AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.Combine(env.ContentRootPath, "App_Data"));
 
             // serve static linked files (JavaScript and CSS for the editor)
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
-                   System.IO.Path.Combine(System.IO.Path.GetDirectoryName(
-                       System.Reflection.Assembly.GetEntryAssembly().Location),
-                       "TXTextControl.Web")),
-                RequestPath = "/TXTextControl.Web"
-            });
+            app.UseStaticFiles();
 
             // enable Web Sockets
             app.UseWebSockets();
